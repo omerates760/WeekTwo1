1be42f5a65bd7ee8c110de64d7292102fa1eb004
Ömer Ateş <omerates760@gmail.com>
2018-07-21 13:00:21 +0300

    add new rows

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,6 +1,26 @@
-fun main(args: ArrayList<String>){
-    print("sdsad")
+fun main(args: Array<String>){
 
+    //Char ile string ifadenin disiziyle elemanlara eriş
+val mrb="ahmet -| me |hmet"
+    var item:Char
+    item=mrb[0]
+    print(item)
+    // """ dsdsd  """   olduğu gibi basttırır
+    val rowsText="""
+        *
+    *      *
+ * * * * * * *
+ *           *
+ *           *
+ * * * * * * *
 
+    """
+    print(rowsText)
+    // METNİ BİR YERİNİ KESMEK İÇİN TRIMMARGIN:
+    print(mrb.trimMargin("ahmet"))
 
+    //Compare to --->metin eşleştirme için kullanılır
+    //sunsequence --> baş ve bitiş index verilir-siler
+    // .? kullanmak yerine  !! kullan
+    
 }
